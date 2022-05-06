efeb26bbd73922dba5a8218ba47a4d436cadad1a
Neda Taghiparast <nedataghiparast@gmail.com>
2022-05-06 11:03:49 -0700

    DATA: Add Contact Model

---
+++ ContactList.Core/Models/Contacts/Contact.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ContactList.Core.Models.Contacts
+{
+    public class Contact
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public int PhoneNumber { get; set; }
+        public DateTimeOffset CreatedDate { get; set; }
+        public DateTimeOffset UpdatedDate { get; set; }
+    }
+}
