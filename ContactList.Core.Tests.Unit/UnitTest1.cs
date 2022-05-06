8a02ba37f2999fdc61dae6415655beef180c4363
Neda Taghiparast <nedataghiparast@gmail.com>
2022-05-06 10:50:11 -0700

    INITIAL: Add Core And Test Project

---
+++ ContactList.Core.Tests.Unit/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace ContactList.Core.Tests.Unit
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ ContactList.Core/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace ContactList.Core.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<string> Get() =>
+            Ok("HelloWorld");
+       
+    }
+}
